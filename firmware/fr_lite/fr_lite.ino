fb224dbfabf0ce917d55a86ba479d4fa4143c635
Josh Boquiren <thejoshbq@proton.me>
2026-09-16 14:56:18 -0400

    release: v3.4.0-alpha.12

--- firmware/fr_lite/fr_lite.ino
+++ firmware/fr_lite/fr_lite.ino
@@ -90,7 +90,7 @@ void onLeverRelease(DeviceType source) {
 }
 
 void SendIdentification() {
-  Serial.println(F("{\"level\":\"000\",\"device\":\"CONTROLLER\",\"sketch\":\"fr_lite.ino\",\"version\":\"v3.4.0-alpha.11\",\"baud_rate\":115200,\"schedule\":\"FIXED_RATIO\"}"));
+  Serial.println(F("{\"level\":\"000\",\"device\":\"CONTROLLER\",\"sketch\":\"fr_lite.ino\",\"version\":\"v3.4.0-alpha.12\",\"baud_rate\":115200,\"schedule\":\"FIXED_RATIO\"}"));
 }
 
 void setup() {
