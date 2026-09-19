d922d0cee223a317bbe6b11854039f11f420274d
Josh Boquiren <thejoshbq@proton.me>
2026-09-19 13:25:45 -0400

    release: v3.4.0-beta.1

--- firmware/fr/fr.ino
+++ firmware/fr/fr.ino
@@ -97,7 +97,7 @@ void onLeverRelease(DeviceType source) {
 }
 
 void SendIdentification() {
-  Serial.println(F("{\"level\":\"000\",\"device\":\"CONTROLLER\",\"sketch\":\"fr.ino\",\"version\":\"v3.4.0-alpha.12\",\"baud_rate\":115200,\"schedule\":\"FIXED_RATIO\"}"));
+  Serial.println(F("{\"level\":\"000\",\"device\":\"CONTROLLER\",\"sketch\":\"fr.ino\",\"version\":\"v3.4.0-beta.1\",\"baud_rate\":115200,\"schedule\":\"FIXED_RATIO\"}"));
 }
 
 void setup() {
